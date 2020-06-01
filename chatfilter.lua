6d02cda06ce10d9b421e193ed5f9c2405d8b3bf3
f90 <no-reply>
2020-06-01 11:29:25 +0100

    Typo

--- chatfilter.lua
+++ chatfilter.lua
@@ -1,6 +1,6 @@
 local function hideInviteWhispers(self, event, msg, author, ...)
     if RIC_CodewordHide then
-        return RIC_Codewords_Handler.equalsCodeword(msg) -- If our message exactly constains the codeword and nothing else, hide it!
+        return RIC_Codewords_Handler.equalsCodeword(msg) -- If our message exactly contains the codeword and nothing else, hide it!
     else
         return false -- Always show messages if we are not hiding codewords
     end
